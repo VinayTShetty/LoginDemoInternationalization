ab40ca6769044798a1c04db3f422a8177dff5633
VinayTShetty <vinaytshetty@gmail.com>
2023-04-27 13:51:08 +0530

    Code Cleaning Removed unwanted code of second Route.

--- LoginForm/lib/main.dart
+++ LoginForm/lib/main.dart
@@ -181,24 +181,3 @@ class _MyAppState extends State<MyApp1> {
       );
   }
 }
-
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
